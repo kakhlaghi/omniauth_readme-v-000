4632e822beba1c45bd36457cffdf606561b54d1d
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-31 23:51:07 +0000

    automatically backed up by learn

--- db/migrate/20181031234929_create_users.rb
+++ db/migrate/20181031234929_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
         t.string = :name
         t.string = :email
-        t.
+        t.inteher = :uid
     end
   end
 end
